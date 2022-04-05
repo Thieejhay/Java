3c89eadd988fbc9bac90f4d3d2cf204a7c8b1a6c
Thieejhay <101231923+Thieejhay@users.noreply.github.com>
2022-04-05 20:05:30 +0100

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,36 @@
+var first = prompt("first number");
+var second = prompt("second number");
+
+function sum(first, second) {
+	return Number(first) + Number(second);
+}
+
+alert(sum(first, second))
+
+var first = prompt("first number");
+var second = prompt("second number");
+
+function multiply(first, second) {
+	return Number(first) * Number(second);
+}
+
+alert(multiply(first, second))
+
+var first = prompt("first number");
+var second = prompt("second number");
+
+function subtract(first, second) {
+	return Number(first) - Number(second);
+}
+
+alert(subtract(first, second))
+
+var first = prompt("first number");
+var second = prompt("second number");
+
+
+function subtract(first, second) {
+	return Number(first) - Number(second);
+}
+
+alert(subtract(first, second))
